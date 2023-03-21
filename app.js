72dc93038dfec6ca6031d53eaa231aaf33f75991
brea-practice <ihk.gitcomm@gmail.com>
2023-03-22 00:02:34 +0100

    DEPLOYING

--- app.js
+++ app.js
@@ -41,9 +41,6 @@ app.use("/", quickLinkRoutes)
 const newListing = require("./routes/Product.routes")
 app.use("/", newListing)
 
-const newCart = require("./routes/Cart.routes")
-app.use("/", newCart)
-
 // ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
 require("./error-handling")(app)
 
